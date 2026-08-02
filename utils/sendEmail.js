afea40b773ef5a315f68eaa53d9b27c8a3c54347
Raghav Bhanot <rbraghav05@gmail.com>
2026-08-02 07:24:08 +0530

    fix: switch to Brevo HTTP API instead of SMTP

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,29 +1,31 @@
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  host: process.env.BREVO_HOST,
-  port: Number(process.env.BREVO_PORT),       // string -> number
-  secure: process.env.BREVO_SECURE === "true", // "true" string ko boolean me convert
-  auth: {
-    user: process.env.BREVO_USER,
-    pass: process.env.BREVO_PASS,
-  },
-  connectionTimeout: 10000, // 10 sec me fail ho jaye, latak na rahe
-});
+const axios = require("axios");
 
 const sendEmail = async ({ to, subject, html }) => {
   try {
-    const result = await transporter.sendMail({
-      from: `"ShopZen" <${process.env.BREVO_FROM}>`,
-      to,
-      subject,
-      html,
-    });
+    const response = await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: { name: "ShopZen", email: process.env.BREVO_FROM },
+        to: [{ email: to }],
+        subject,
+        htmlContent: html,
+      },
+      {
+        headers: {
+          "api-key": process.env.BREVO_API_KEY,
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    );
 
-    console.log("Email sent successfully:", result.messageId);
-    return result;
+    console.log("Email sent successfully:", response.data.messageId);
+    return response.data;
   } catch (error) {
-    console.error("Error sending email:", error.message);
+    console.error(
+      "Error sending email:",
+      error.response ? error.response.data : error.message
+    );
     throw error;
   }
 };
